bf3f60b373241d4e35775a9ab9caf38f612faae1
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-05-19 02:08:42 +0600

    add footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,12 +2,14 @@
 
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/Navbar/Navbar";
+import Footer from "../pages/shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div className="container mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
