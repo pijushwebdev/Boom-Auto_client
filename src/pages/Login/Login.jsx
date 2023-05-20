c987c3be80fe8fac08044a16e83c6bd958dce40c
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-05-21 04:16:26 +0600

    login and register style

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -61,25 +61,25 @@ const Login = () => {
             .catch(error => toast.error(error.message))
     }
     return (
-        <div className="flex justify-center items-center h-screen background-style">
-            <div>
-
+        <div className="inset-shadow grid grid-cols-1 md:grid-cols-2 gap-3">
+            <div className='p-10 order-2 inset-shadow hover:shadow-xl'>
+                <img className='hover:shadow-lg' src="https://i.ibb.co/Ldb8ZC8/4957412-Mobile-login-Cristina.jpg" alt="" />
             </div>
-            <div>
-                <form onSubmit={handleSignIn} className='flex flex-col shadow-md p-10 items-center '>
+            <div className='inset-shadow hover:shadow-xl'>
+                <form onSubmit={handleSignIn} className='hover:shadow-xl flex h-full flex-col shadow-md p-10 justify-center items-center '>
 
-                    <input className='py-1 px-3 rounded-md bg-cyan-800 text-white outline-none' ref={emailRef} type="email" name='email' id='email' placeholder='Enter email' required />
-                    <input className='my-5 py-1 px-3 rounded-md bg-cyan-800 text-white outline-none' type="password" name='password' id='password' placeholder='Enter password' required />
+                    <input className='py-2 w-1/2  px-3 font-bold rounded-md bg-[#107aa7] text-[#dfe2ec] outline-none' ref={emailRef} type="email" name='email' id='email' placeholder='Enter email' required />
+                    <input className='my-5 py-2 w-1/2 px-3 font-bold rounded-md bg-[#107aa7] text-[#dfe2ec] outline-none' type="password" name='password' id='password' placeholder='Enter password' required />
 
 
-                    <input className='py-2 px-3 cursor-pointer border rounded-md' type="submit" value='SignIn' />
+                    <input className=' font-bold text-lg mb-10 py-2 px-3 w-1/2 border-[#03a3e6] cursor-pointer border rounded-md' type="submit" value='SignIn' />
 
 
                     <p><small>Forget Password? <button onClick={handlePasswordReset} className="hover:border-b-2 hover:border-slate-500 border-b-2 border-transparent transition-all duration-500">Reset</button></small></p>
-                    <p><small>No account? <Link className="hover:border-b-2 hover:border-slate-500 border-b-2 border-transparent transition-all duration-500" to='/register'>SignUp</Link></small></p>
+                    <p><small>No account? <Link className="hover:border-b-2 border-[#03a3e6] hover:border-slate-500 border-b-2 border-transparent transition-all duration-500" to='/register'>SignUp</Link></small></p>
 
                     <div className='mt-5'>
-                        <button onClick={handleGoogleSignIn} className='rounded-md py-2 px-3 border mr-5' ><FaGoogle className="inline mr-2" />Sign in with Google</button>
+                        <button onClick={handleGoogleSignIn} className='rounded-md w-full font-bold text-[#294090] py-2 px-3 border-[#03a3e6] border mr-5' ><FaGoogle className="inline mr-2" />Sign in with Google</button>
                     </div>
 
                 </form>
