436411624aa2d3c54eae7f66d404719abfdb89ac
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-05-22 14:24:40 +0600

    shop now button link set

--- src/pages/Home/Home/Banner.jsx
+++ src/pages/Home/Home/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Banner.css';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -8,7 +9,7 @@ const Banner = () => {
                 <div className=' w-full h-full flex flex-col justify-between bottom-4 left-4 md:bottom-1/4 md:left-1/4 bg-[#241d1d7a]'>
                     <h1 className='text-center lg:text-7xl text-3xl font-bold break-words text-[#239abe]'>BooM Auto</h1>
                     <p className='text-lg font-semibold my-3 text-center text-cyan-600'>Get Exclusive Discount..</p>
-                    <button className='md:py-2 md:px-3 py-1 px-2 btn-hover  bottom-10 left-12 md:left-20 text-lg font-semibold rounded-md bg-[#293f9062] text-[#68dcff]'>Shop Now</button>
+                    <Link to='/allToys' className='md:py-2 text-center md:px-3 py-1 px-2 btn-hover  bottom-10 left-12 md:left-20 text-lg font-semibold rounded-md bg-[#293f9062] text-[#68dcff]'>Shop Now</Link>
                 </div>
             </div>
             <div></div>
